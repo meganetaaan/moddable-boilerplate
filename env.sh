48f19bb455cd81f7717034bc70997e77c7797f48
Shinya Ishikawa <ishikawa.s.1027@gmail.com>
2020-07-05 00:36:56 +0900

    Remove unused script

--- env.sh
+++
@@ -1,6 +0,0 @@
-export MODDABLE=~/Projects/moddable
-export IDF_PATH=~/esp32/esp-idf
-export PATH=$PATH:$MODDABLE/build/bin/lin/release
-export PATH=$PATH:$HOME/esp32/xtensa-esp32-elf/bin
-export UPLOAD_PORT=/dev/ttyUSB0
-sudo adduser $USER dialout 
